c6a44b28df4bc6a077930601e541abaf85281911
Rem0o <rem.merc@outlook.com>
2022-10-19 12:43:17 -0400

    Updates

---
+++ component---src-pages-404-tsx-eb6ecb86c6443fd32f29.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfan_control=self.webpackChunkfan_control||[]).push([[218],{2513:function(e,n,t){t.r(n),t.d(n,{Head:function(){return l}});var r=t(7294),u=t(1597);n.default=function(){return r.createElement("main",null,"404 ",r.createElement(u.rU,{to:"/"},"Go home"),".")};var l=function(){return r.createElement("title",null,"Not found")}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-eb6ecb86c6443fd32f29.js.map
